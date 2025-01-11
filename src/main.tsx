c593be09f2dd3fa5566bff3f294d19b2c7ac41bd
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2025-01-11 18:10:16 +0330

    feat: add Material-UI with custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,20 @@ import { createRoot } from 'react-dom/client';
 
 import { store } from './store';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 
-import './index.css';
+import theme from './styles/theme';
 import App from './App.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>
 );
