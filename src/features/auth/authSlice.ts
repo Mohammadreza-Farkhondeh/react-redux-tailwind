de5c3edce65f734a32c13dc81d74e016f1d79687
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2025-01-12 18:59:04 +0330

    feat: implement auth slice uses auth actions

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,80 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { AuthState } from './authTypes';
+
+import { login, register, logout, getCurrentUser } from './authActions';
+
+const initialState: AuthState = {
+  user: null,
+  token: localStorage.getItem('token'),
+  isAuthenticated: false,
+  status: 'idle',
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    resetAuthState: (state) => {
+      state.status = 'idle';
+      state.error = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      // Login
+      .addCase(login.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isAuthenticated = true;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload as string;
+      })
+      // Register
+      .addCase(register.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        //   state.user = action.payload.user;
+        //   state.token = action.payload.token;
+        state.isAuthenticated = true;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload as string;
+      })
+      // Logout
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null;
+        state.token = null;
+        state.isAuthenticated = false;
+        state.status = 'idle';
+      })
+      // Get Current User
+      .addCase(getCurrentUser.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(getCurrentUser.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.user = action.payload.user;
+        state.isAuthenticated = true;
+      })
+      .addCase(getCurrentUser.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload as string;
+        state.isAuthenticated = false;
+      });
+  },
+});
+
+export const { resetAuthState } = authSlice.actions;
+export default authSlice.reducer;
