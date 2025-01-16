7aaa79d0588e7f8f89d3ae5b96574a277dc7bda4
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2025-01-16 16:38:41 +0330

    feat: add SignupPage route to application routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ProtectedRoute } from '../components/layout/ProtectedRoute';
 import { LoginPage } from '../features/auth/pages/LoginPage';
 import Dashboard from '@mui/icons-material/Dashboard';
+import { SignupPage } from '../features/auth/pages/SignupPage';
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
       <Route element={<ProtectedRoute />}>
         <Route path="/dashboard" element={<Dashboard />} />
       </Route>
