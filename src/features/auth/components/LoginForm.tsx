dd527e0e89f02daf8f22d4af642818b7aa06e1e6
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2025-01-17 23:28:46 +0330

    feat: refactor LoginForm to use Formik for form handling and validation, add password visibility toggle and remember me option

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -2,64 +2,149 @@ import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../store';
 import { resetAuthState } from '../authSlice';
 import { login } from '../authActions';
-import type { LoginRequest } from '../authTypes';
-import { Alert } from '../../../components/common/Alert';
-import { TextField } from '../../../components/common/TextField';
-import { Button } from '../../../components/common/Button';
+import type { TokenObtainPairRequest } from '../authTypes';
+
+import {
+  Checkbox,
+  FormControlLabel,
+  IconButton,
+  InputAdornment,
+  Typography,
+  Stack,
+  Box,
+  Alert,
+  TextField,
+  Button,
+} from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 
 export const LoginForm = () => {
   const dispatch = useAppDispatch();
   const { status, error } = useAppSelector((state) => state.auth);
-  const [credentials, setCredentials] = useState<LoginRequest>({
-    username: '',
-    password: '',
-  });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCredentials((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }));
+  const [showPassword, setShowPassword] = useState(false);
+  const [rememberMe, setRememberMe] = useState(true);
+
+  const handleClickShowPassword = () => {
+    setShowPassword((prev) => !prev);
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleMouseDownPassword = (event: React.MouseEvent) => {
+    event.preventDefault();
+  };
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required('Username is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const handleSubmit = async (values: TokenObtainPairRequest) => {
     dispatch(resetAuthState());
-    await dispatch(login(credentials));
+    await dispatch(login(values));
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {error && <Alert severity="error">{error}</Alert>}
-      <TextField
-        fullWidth
-        margin="normal"
-        label="Username"
-        name="username"
-        type="text"
-        value={credentials.username}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        fullWidth
-        margin="normal"
-        label="Password"
-        name="password"
-        type="password"
-        value={credentials.password}
-        onChange={handleChange}
-        required
-      />
-      <Button
-        fullWidth
-        type="submit"
-        variant="contained"
-        disabled={status === 'loading'}
-        sx={{ mt: 2 }}
-      >
-        {status === 'loading' ? 'Logging in...' : 'Login'}
-      </Button>
-    </form>
+    <Formik
+      initialValues={{ username: '', password: '' }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        isSubmitting,
+      }) => (
+        <Form>
+          {error && <Alert severity="error">{error}</Alert>}
+
+          {/* Username Field */}
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Username"
+            name="username"
+            value={values.username}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.username && Boolean(errors.username)}
+            helperText={touched.username && errors.username}
+            required
+          />
+
+          {/* Password Field */}
+          <TextField
+            fullWidth
+            margin="normal"
+            label="Password"
+            name="password"
+            type={showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.password && Boolean(errors.password)}
+            helperText={touched.password && errors.password}
+            required
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          {/* Remember Me and Forgot Password */}
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+            mt={2}
+          >
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={rememberMe}
+                  onChange={(e) => setRememberMe(e.target.checked)}
+                  name="rememberMe"
+                />
+              }
+              label="Remember me"
+            />
+            <Typography
+              variant="subtitle1"
+              color="secondary"
+              sx={{ textDecoration: 'none', cursor: 'pointer' }}
+            >
+              Forgot Password?
+            </Typography>
+          </Stack>
+
+          {/* Submit Button */}
+          <Box mt={3}>
+            <Button
+              fullWidth
+              type="submit"
+              variant="contained"
+              disabled={isSubmitting || status === 'loading'}
+            >
+              {isSubmitting || status === 'loading' ? 'Logging in...' : 'Login'}
+            </Button>
+          </Box>
+        </Form>
+      )}
+    </Formik>
   );
 };
