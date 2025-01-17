d82e8b8abe8df5cd8537779337238d55701b8f29
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2025-01-17 23:28:08 +0330

    feat: remove unused common components (Alert, Avatar, Badge, Button, Input, TextField, Typography) to streamline the codebase

--- src/components/common/Alert/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Alert as MuiAlert, AlertProps as MuiAlertProps } from '@mui/material';
-
-interface AlertProps extends Omit<MuiAlertProps, 'variant'> {
-  variant?: 'success' | 'error' | 'warning' | 'info';
-}
-
-export const Alert = ({ variant = 'info', ...props }: AlertProps) => (
-  <MuiAlert severity={variant} {...props} />
-);
