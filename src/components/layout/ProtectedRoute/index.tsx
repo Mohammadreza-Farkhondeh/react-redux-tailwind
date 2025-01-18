9ee86813385cbcd1288d789cbc5bf6ab1c58a9d2
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2025-01-18 21:47:20 +0330

    feat: remove Mui components to streamline the codebase for tailwind

--- src/components/layout/ProtectedRoute/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAppSelector } from '../../../store';
-
-export const ProtectedRoute = () => {
-  const { isAuthenticated } = useAppSelector((state) => state.auth);
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-};
