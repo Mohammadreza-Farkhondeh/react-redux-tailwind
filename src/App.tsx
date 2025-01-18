c86ca9688f4343389c57abe1e697f89cc2bd7dca
Mohammadreza-Farkhondeh <mohammr.farkhonde12@sharif.edu>
2025-01-18 22:09:01 +0330

    feat: remove Material-UI theme provider and CssBaseline to simplify App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import theme from './styles/theme';
 
 import router from './routes';
 import { store } from './store';
@@ -10,10 +7,7 @@ import { store } from './store';
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <RouterProvider router={router} />
     </Provider>
   );
 }
